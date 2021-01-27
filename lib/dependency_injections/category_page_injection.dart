443710e34538d37ef9229ae77f108874e3650e93
Douglas Valerio <douglas.valerio@matilha.digital>
2021-01-27 12:38:14 -0300

    fixing dependency injections

--- lib/dependency_injections/category_page_injection.dart
+++ lib/dependency_injections/category_page_injection.dart
@@ -2,14 +2,10 @@ import '../features/categories_itens_page/data/repositories/main_themes_item_rep
 import '../features/categories_itens_page/data/sources/main_themes_item_remote_data_source.dart';
 import '../features/categories_itens_page/domain/repositories/main_themes_item_repository.dart';
 import '../features/categories_itens_page/domain/usecases/get_main_themes_item_list.dart';
-import '../features/categories_itens_page/presentation/bloc/main_theme_item_bloc.dart';
 
 import '../injection_container.dart';
 
 Future<void> categoriesPageInjection() async {
-  //Features layer
-  serviceLocator.registerFactory(
-      () => MainThemeItemBloc(getMainThemesItemList: serviceLocator()));
   serviceLocator.registerLazySingleton(
     () => GetMainThemesItemList(serviceLocator()),
   );
